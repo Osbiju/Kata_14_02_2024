8d0f65e171ca3fcc9bb43cccc2120cdacfb761af
Oscar <okeshik@protonmail.com>
2024-02-14 13:14:07 +0100

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+/*
+Crea una función que reciba dos parámetros para crear una cuenta atrás.
+El primero, representa el número en el que comienza la cuenta.
+El segundo, los segundos que tienen que transcurrir entre cada cuenta.
+Sólo se aceptan números enteros positivos.
+El programa finaliza al llegar a cero.
+Debes imprimir cada número de la cuenta atrás.
+ */
+
         Scanner entrada = new Scanner(System.in);
         System.out.println("dime el primer numero para la cuenta atras");
         int inicio = entrada.nextInt();
